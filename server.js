3fe915bc11ceedd437c7ae9246133c566ddd030d
Glitch (tungsten-halved-snipe) <none>
2023-02-25 22:18:33 +0000

    🎧⚡ Updated with Glitch

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.use((req, res, next) => {
     next()
 })
 
+run();
+
 async function run() { 
 browser = await puppeteer.launch({
       args: ['--no-sandbox', '--start-maximized'],
@@ -36,47 +38,12 @@ browser = await puppeteer.launch({
       headless: true,
       defaultViewport: null
 }) 
-  console.log('init browser');
   }
-// app.get("/", async (request, response) => {
-//   try {
-//     await run();
-//     const page = await browser.newPage();
-//     if(cookies) {
-//     	await page.setCookie(...cookies); 
-//    } 
-//    await page.goto('https://www.touch.com.lb/autoforms/portal/touch/mytouch/pinrecharge',{waitUntil: 'networkidle0'});
-//     await page.setViewport({ width: 0, height: 0 });
-    
-//    const logged = await page.$eval('input[name=frmGSM]', () => true).catch(() => false)
-//                 console.log(logged)
-//     if(!logged) {
-//     console.log('logging');
-//    await page.focus('#user')
-//  await page.keyboard.type(process.env.user)
-//   await page.focus('#pass')
-//  await page.keyboard.type(process.env.pass)
-    
-//    await page.$eval('input[name=imageField]', el => el.click());
-//    //await page.waitForNavigation();
-   
-//    cookies = await page.cookies(); 
-//   } else {
-//   	console.log('youre logged in btw') 
-//  } 
-   
-//     await page.screenshot({path: __dirname+'/public/puppeteer.png'});
 
-//     await browser.close();
-//    response.sendFile(__dirname+'/public/puppeteer.png');
+
+app.post('/charge-mtc', async (req, res) => {
   
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-
-app.post('/charge', async (req, res) => {
+  console.log('mtc-charge')
 	
   try {
    const {
@@ -124,7 +91,7 @@ app.post('/charge', async (req, res) => {
     // await page.click('input[type="submit"]');
     // await page.$eval('input[type=submit]', el => el.click());
     
-    await page.screenshot({path: __dirname+'/public/puppeteer.png'});
+    // await page.screenshot({path: __dirname+'/public/puppeteer.png'});
     
     
     await page.$eval('form[name="ThirdPartyRechargeForm"]', form => form.submit());
@@ -133,8 +100,72 @@ app.post('/charge', async (req, res) => {
  
     
     
-    await browser.close();
+    await page.close();
   // res.sendFile(__dirname+'/public/puppeteer.png');
+    res.json({
+      msg: 'the request has been sent successfully' 
+    })
+    
+  } catch (error) {
+    console.log(error);
+    res.json({
+     msg: `an error occurred ${error.message}`
+     }) 
+  }
+})
+
+
+app.post('/charge-alfa', async (req, res) => {
+  
+  console.log('alfa-charge')
+	
+  try {
+   const {
+    num,
+    pin
+  } = req.body;
+         
+    await run();
+    
+    const page = await browser.newPage();
+    
+    await page.goto('https://www.alfa.com.lb/en/account/pay-and-recharge/prepaid/scratch-card',{waitUntil: 'networkidle0'});
+    await page.setDefaultNavigationTimeout(0);
+    await page.setViewport({ width: 0, height: 0 });
+    
+   
+    
+    await page.focus('input#Number')
+    await page.keyboard.type(num)
+    
+    await page.focus('input#NumberConfirm')
+    await page.keyboard.type(num)
+    
+    await page.focus('input#RechargeCode')
+    await page.keyboard.type(pin)
+    
+    await page.waitForTimeout(3000);
+    await page.$eval('form', form => form.submit());
+    //await page.click("button[type=submit]");
+     
+    // await page.keyboard.press('Enter');
+    
+    // await page.$eval('form.form', form => form.submit());
+    
+    // await page.waitForSelector('button[type=submit]');
+    // await page.focus('button[type=submit]')
+    // await page.click('button[type=submit]');  
+    
+    
+    // await page.waitForNavigation()
+    
+    await page.screenshot({path: __dirname+'/public/puppeteer.png'});
+    
+    
+  
+    
+    
+    await page.close();
     res.json({
       msg: 'the request has been sent successfully' 
     })
